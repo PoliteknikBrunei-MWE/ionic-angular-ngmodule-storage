e4bffaee66325335eea73ce061d142dfc18f8aa1
Bahit Hamid <bahit.hamid@gmail.com>
2024-02-19 07:50:01 +0800

    Update saveUserName method to accept an object instead of a string

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,21 +7,21 @@ import { Storage } from '@ionic/storage-angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  memberName!: string;
+  memberName!: {fname: string, lname: string};
+  firstName!: string;
 
   constructor(private storage: Storage) {
   }
 
-  saveUserName(uname: string) {
-    this.storage.set('username', uname);
+  saveUserName(uname: {fname: string, lname: string}) {
+    this.storage.set('username', JSON.stringify(uname));
   }
 
   getUserName() {
     this.storage.get('username').then((data) => {
       if(data){
-        this.memberName = data;
-      } else {
-        this.memberName = "Guest";
+        this.memberName = JSON.parse(data);
+        this.firstName = this.memberName.fname;
       }
     });
   }
